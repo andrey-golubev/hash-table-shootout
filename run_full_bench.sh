beecba2654252caaf2bfda0f35e53e227d49829d
Andrei Golubev <andrei.golubev@qt.io>
2020-11-03 16:50:27 +0100

    Run bench with priority

--- run_full_bench.sh
+++ run_full_bench.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+. ./set_env.sh
 make clean
 make -j$(nproc)
-python bench.py
+LLP=${LD_LIBRARY_PATH}
+LPR=${LD_PRELOAD}
+sudo -E nice -n-20 ionice -c1 -n0 sudo -u $USER LD_LIBRARY_PATH=${LLP} LD_PRELOAD=${LPR} python bench.py
 ./generate_html.sh
