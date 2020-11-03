6fe56da6897231ec93ddacd2e60c9aaeb4436dd0
Andrei Golubev <andrei.golubev@qt.io>
2020-11-03 13:29:30 +0100

    Fix set_env.sh script

--- set_env.sh
+++ set_env.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-export LD_LIBRARY_PATH=${HOME}/work/qt5/qtbase/lib/:${HOME}/work/5.15/qtbase/lib/:${LD_LIBRARY_PATH}
+export LD_LIBRARY_PATH=${HOME}/work/build/qt5/qtbase/lib/:${HOME}/work/build/5.15/qtbase/lib/:${LD_LIBRARY_PATH}
 export LD_PRELOAD=~/work/build/5.15/qtbase/lib/libQt5Core.so
