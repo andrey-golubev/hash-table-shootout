aa0bdc9fc4939526a6d4fe8732587fc0d26b0e8f
Andrei Golubev <andrei.golubev@qt.io>
2020-11-03 12:40:17 +0100

    Generate charts with python

--- make_chart_data.py
+++ make_chart_data.py
@@ -24,78 +24,25 @@ proper_names = OrderedDict([
     ('qlist_qt6', 'QList (dev)'),
     ('qlist_qt515', 'QList (5.15)'),
     ('qvector_qt515', 'QVector (5.15)'),
-
-    # ('std_unordered_map', 'std::unordered_map'),
-    # ('google_dense_hash_map', 'google::dense_hash_map'),
-    # ('qt_qhash', 'QHash'),
-    # ('tsl_sparse_map', 'tsl::sparse_map'),
-    # ('tsl_hopscotch_map', 'tsl::hopscotch_map'),
-    # ('tsl_robin_map', 'tsl::robin_map'),
-    # ('tsl_hopscotch_map_store_hash', 'tsl::hopscotch_map (with StoreHash)'),
-    # ('tsl_robin_map_store_hash', 'tsl::robin_map (with StoreHash)'),
-    # ('tsl_hopscotch_map_mlf_0_5', 'tsl::hopscotch_map (0.5 mlf)'),
-    # ('tsl_robin_map_mlf_0_9', 'tsl::robin_map (0.9 mlf)'),
-    # ('tsl_ordered_map', 'tsl::ordered_map'),
-    # ('tsl_robin_pg_map', 'tsl::robin_pg_map'),
-    # ('ska_flat_hash_map', 'ska::flat_hash_map'),
-    # ('google_dense_hash_map_mlf_0_9', 'google::dense_hash_map (0.9 mlf)'),
-    # ('ska_flat_hash_map_power_of_two', 'ska::flat_hash_map (power of two)'),
-    # ('google_sparse_hash_map', 'google::sparse_hash_map'),
-    # ('boost_unordered_map', 'boost::unordered_map'),
-    # ('spp_sparse_hash_map', 'spp::sparse_hash_map'),
-    # ('emilib_hash_map', 'emilib::HashMap'),
-    # ('tsl_array_map', 'tsl::array_map'),
-    # ('tsl_array_map_mlf_1_0', 'tsl::array_map (1.0 mlf)'),
 ])
 
 # do them in the desired order to make the legend not overlap the chart data
 # too much
 program_slugs = [
     'std_vector',
-    'std_vector515',
     'qlist_qt6',
     'qlist_qt515',
     'qvector_qt515',
-
-    # 'std_unordered_map',
-    # 'google_dense_hash_map',
-    # 'qt_qhash',
-    # 'tsl_sparse_map',
-    # 'tsl_hopscotch_map',
-    # 'tsl_robin_map',
-    # 'tsl_hopscotch_map_store_hash',
-    # 'tsl_robin_map_store_hash',
-    # 'tsl_hopscotch_map_mlf_0_5',
-    # 'tsl_robin_map_mlf_0_9',
-    # 'tsl_ordered_map',
-    # 'tsl_robin_pg_map',
-    # 'ska_flat_hash_map',
-    # 'google_dense_hash_map_mlf_0_9',
-    # 'ska_flat_hash_map_power_of_two',
-    # 'google_sparse_hash_map',
-    # 'boost_unordered_map',
-    # 'spp_sparse_hash_map',
-    # 'emilib_hash_map',
-    # 'tsl_array_map',
-    # 'tsl_array_map_mlf_1_0',
+    'std_vector515',
 ]
 
-# hashmap which will be shown (checkbox checked)
+# programs which will be shown (checkbox checked)
 default_programs_show = [
     'std_vector',
     'std_vector515',
     'qlist_qt6',
     'qlist_qt515',
     'qvector_qt515',
-
-    # 'std_unordered_map',
-    # 'google_dense_hash_map',
-    # 'qt_qhash',
-    # 'tsl_sparse_map',
-    # 'tsl_hopscotch_map',
-    # 'tsl_robin_map',
-    # 'tsl_hopscotch_map_store_hash',
-    # 'tsl_robin_map_store_hash'
 ]
 
 chart_data = {}
--- make_html.py
+++ make_html.py
@@ -1,5 +1,57 @@
 #!/usr/bin/env python
 
 import sys
+
+op2html = {
+    'append_': '<h3>Append one element: execution time</h3></br><p>Appending into container, without prior space reservation.</p></br>',
+    'prepend_': '<h3>Prepend one element: execution time</h3></br><p>Prepending into container, without prior space reservation.</p></br>',
+    'insert1_mid_': '<h3>Insert one element in the middle: execution time</h3></br><p>Inserting into container, without prior space reservation.</p></br>',
+    'insert1_quarter_': '<h3>Insert one element in the first quarter (e.g. pos = size() / 4): execution time</h3></br><p>Inserting into container, without prior space reservation.</p></br>',
+    'insert1_last_quarter_': '<h3>Insert one element in the last quarter (e.g. pos = 3 * size() / 4): execution time</h3></br><p>Inserting into container, without prior space reservation.</p></br>',
+    'access_every_': '<h3>Accessing every element: execution time</h3></br><p>Resize container to specified size, then access each element.</p></br>',
+    'remove_first_': '<h3>Remove first element: execution time</h3></br><p>Resize container to specified size, remove first element in a loop.</p></br>',
+    'remove_mid_': '<h3>Remove middle element: execution time</h3></br><p>Resize container to specified size, remove middle element in a loop.</p></br>',
+    'remove_last_': '<h3>Remove last element: execution time</h3></br><p>Resize container to specified size, remove last element in a loop.</p></br>',
+}
+
+suffix2pos = {
+    'int': '__TEST_DATA_INT_GOES_HERE__',
+    'qstr': '__TEST_DATA_QSTR_GOES_HERE__',
+    'stdstr': '__TEST_DATA_STDSTR_GOES_HERE__',
+    'three_ptrs': '__TEST_DATA_THREE_PTRS_GOES_HERE__',
+}
+
+def construct_table(suffix, op):
+    heading = op2html[op]
+    identifier = op + suffix
+    chart = '<div class="chart" id="%s"></div>' % identifier
+    title = '<div class="xaxis-title">number of entries in container</div>'
+    checkboxes = '<ul class="choices" id="%s_choices"></ul>' % identifier
+    return '\n'.join([heading, '</br>', '\n'.join([chart, title, checkboxes]), '</br>'])
+
+def insert_bench_layout(html_template):
+    ops = [
+        'append_',
+        'prepend_',
+        'insert1_mid_',
+        'insert1_quarter_',
+        'insert1_last_quarter_',
+        'access_every_',
+        'remove_first_',
+        'remove_mid_',
+        'remove_last_',
+    ]
+
+    for suffix in ["int", "qstr", "stdstr", "three_ptrs"]:
+        # construct full table
+        full_table = []
+        for op in ops:
+            minitable = construct_table(suffix, op)
+            full_table.append(minitable)
+        html_template = html_template.replace(suffix2pos[suffix], '\n</br>\n'.join(full_table))
+
+    return html_template
+
 html_template = file('charts-template.html', 'r').read()
+html_template = insert_bench_layout(html_template)
 file('charts.html', 'w').write(html_template.replace('__CHART_DATA_GOES_HERE__', sys.stdin.read()))
